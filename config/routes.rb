60e6e72e96c7437549406ce4063e4b9f059a074a
Dipen Chauhan <get.dipen@gmail.com>
2019-06-25 23:34:49 -0700

    Added home methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/check'
+  get 'home/results'
   resources :connected_accounts
+  get 'connected_accounts/refresh'
   devise_for :users
 
   root 'home#index'
