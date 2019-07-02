9e24c26ed221fe7dd9c856eeb1b7c1702eae0037
Dipen Chauhan <get.dipen@gmail.com>
2019-07-02 00:17:46 -0700

    Added helper for bootstrap/flash and page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # https://gist.github.com/suryart/7418454#gistcomment-970347
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-primary'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  # https://rubyplus.com/articles/3321-Pretty-Page-Title-in-Rails-5
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
 end
