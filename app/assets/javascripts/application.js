9b51199167c25e325091fd3038998df76d5d23ab
Dipen Chauhan <get.dipen@gmail.com>
2019-07-03 22:02:51 -0700

    Added stripe credit card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,47 @@
 //= require_tree .
 
 $(document).on("turbolinks:load", function() {
-  
+  $(".form-check").click(function() {
+    $(this).addClass("is-selected");
+    $(this)
+      .siblings()
+      .removeClass("is-selected");
+    $(this)
+      .children("input[name='exampleRadios']:radio")
+      .eq(0)
+      .prop("checked", true);
+  });
+
+  // Create a Stripe client.
+  var stripe = Stripe("pk_test_gEtbe0LdmNqijbYBOZUMe9kx");
+
+  // Create an instance of Elements.
+  var elements = stripe.elements();
+
+  var style = {
+    base: {
+      iconColor: "#c4f0ff",
+      color: "#fff",
+      fontWeight: 500,
+      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+      fontSize: "16px",
+      fontSmoothing: "antialiased",
+      ":-webkit-autofill": {
+        color: "#fce883"
+      },
+      "::placeholder": {
+        color: "#87BBFD"
+      }
+    },
+    invalid: {
+      iconColor: "#FFC7EE",
+      color: "#FFC7EE"
+    }
+  };
+
+  // Create an instance of the card Element.
+  var card = elements.create("card", { style: style });
+
+  // Add an instance of the card Element into the `card-element` <div>.
+  card.mount("#card-element");
 });
