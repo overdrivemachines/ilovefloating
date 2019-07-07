2b2f1c9d282e854c3deb4504c4044d9d904d5a7d
Dipen Chauhan <get.dipen@gmail.com>
2019-07-06 18:18:31 -0700

    Incrementing installments_paid

--- app/services/stripe/invoice_event_handler.rb
+++ app/services/stripe/invoice_event_handler.rb
@@ -16,11 +16,54 @@ module Stripe
       puts "***handle_invoice_payment_failed executing"
     end
 
+    # Execute only for `invoice.payment_succeeded` events.
     def handle_invoice_payment_succeeded(event)
       puts "****handle_invoice_payment_succeeded executing"
+      # TODO: logging
       # logger.info "handle_invoice_payment_succeeded executing"
       # logger.info "EVENT:#{event.type}:#{event.id}"
       
+      # Increment payments count
+
+      # https://stripe.com/docs/recipes/installment-plan
+      # https://stripe.com/docs/webhooks/setup
+      
+      # Grab the subscription line item.
+      sub =  event.data.object.lines.data[0]
+
+      # Execute only for installment plans.
+      if !sub.metadata[:installments_paid].nil?
+      # Recommendation: Log invoices and check for duplicate events.
+      # Recommendation: Note that we send $0 invoices for trials.
+      #                 You can verify the `amount_paid` attribute of
+      #                 the invoice object before incrementing the count.
+
+          # Retrieve and increment the number of payments.
+          count = sub.metadata[:installments_paid].to_i
+          count += 1
+          # Metadata is not write-protected; creating a database is an alternative.
+
+          # Save incremented value to `installments_paid` metadata of the subscription.
+          subscription_object = Stripe::Subscription.update(
+            sub[:subscription],
+            {
+              metadata: {
+                installments_paid: count,
+              },
+            }
+          )
+
+          # Check if all 10 installments have been paid.
+          # If paid in full, then cancel the subscription.
+          if count >= 10
+              subscription_object.delete
+          end
+      end
+
+
+
+
+
       puts event.data.object.class
       render json: {:status => 400, :info => event}
     end
