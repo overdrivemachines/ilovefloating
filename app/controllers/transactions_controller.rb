fbf71a617cf026ed0abe575d58e0822741ea6de5
Dipen Chauhan <get.dipen@gmail.com>
2019-07-08 15:09:41 -0700

    Moved create customer, calculating fees to functions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -58,79 +58,55 @@ class TransactionsController < ApplicationController
         return
       end
 
-      # Determine the Application Fee amount
-      stripe_fee = (0.3 + (2.9 / 100.0) * @transaction.price).round(2)
-      client_received = @transaction.price - stripe_fee
-      if (client_received <= 0)
-        redirect_to connected_accounts_url, flash: { error: "Client made negative amount so no Application Fee received."}
-        return
-      end
-      application_fee = ((connected_account.commission / 100.0) * client_received).round(2)
-
-      # Determine the Connected Account's 
-
-      # Stripe.api_key = 'sk_test_lPVHKFQDPSSLI6uiJr4dVdY7'
+      # Set the API key based on Rails environment
       Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:secret_key]
-      # Stripe.api_key = Rails.application.credentials.stripe[:production][:secret_key]
-      # Stripe.api_key = Rails.application.credentials.api_key
 
-      # finding if customer exists in connected account
-      customer_id = nil
-      begin
-        # customers = Stripe::Customer.list({
-        #     email: @transaction.email,
-        #     limit: 1
-        # }, stripe_account: connected_account.sid)
-        customers = Stripe::Customer.list({
-            email: @transaction.email,
-            limit: 1
-        })
-        if (customers["data"].size == 0)
-          # Customer not found
-          # Create a new Customer
-          puts "Creating new Customer"
-          # customer = Stripe::Customer.create({
-          #   name: @transaction.name,
-          #   email: @transaction.email,
-          #   phone: @transaction.phone,
-          #   source: stripe_token, # obtained with Stripe.js
-          # }, stripe_account: connected_account.sid)
-          customer = Stripe::Customer.create({
-            name: @transaction.name,
-            email: @transaction.email,
-            phone: @transaction.phone,
-            source: stripe_token, # obtained with Stripe.js
-          })
-          customer_id = customer["id"];
-          puts "New Customer ID: " + customer_id
-        else
-          # Customer already exists, get the Customer ID
-          puts "Customer #{@transaction.name} already exists"
-          customer_id = customers["data"][0]["id"]
-          puts "Customer ID: " + customer_id
-          # Update Name and Phone
-          # Stripe::Customer.update(
-          #   customer_id,
-          #   {
-          #     name: @transaction.name,
-          #     email: @transaction.email,
-          #     phone: @transaction.phone,
-          #     source: stripe_token # obtained with Stripe.js
-          #   }, stripe_account: connected_account.sid)
-          Stripe::Customer.update(
-            customer_id,
-            {
-              name: @transaction.name,
-              email: @transaction.email,
-              phone: @transaction.phone,
-              source: stripe_token # obtained with Stripe.js
-            })
-          puts "Customer Updated"
+      # Create an Customer on Connected Account
+      customer_id = create_or_update_customer(@transaction.name, 
+        @transaction.email, 
+        @transaction.phone,
+        @transaction.sales_rep_name,
+        stripe_token,
+        connected_account.sid)
+
+      if is_subscription
+        # Check or Create Product
+        # Check or Create Plan
+        plan_id = check_or_create_plan(connected_account.sid)
+      else
+        stripe_fee = calculate_stripe_fee(@transaction.price)
+        application_fee = calculate_application_fee(@transaction.price, connected_account.commission)
+
+        # Charge the customer
+        # Testing errors:
+        # https://stripe.com/docs/testing
+        # Receipts: https://stripe.com/docs/receipts
+        # https://stripe.com/docs/recipes/sending-custom-email-receipts
+        # Metadata: https://stripe.com/docs/charges
+        begin
+          charge = Stripe::Charge.create({
+            amount: (@transaction.price * 100).to_i,
+            currency: "usd",
+            description: @transaction.item,
+            # source: customer_token.id,
+            statement_descriptor: @transaction.item[0..21],
+            receipt_email: @transaction.email,
+            application_fee_amount: (application_fee * 100).to_i,
+            customer: customer_id,
+            metadata: {'name' => @transaction.name,
+                        'email' => @transaction.email,
+                        'phone' => @transaction.phone,
+                        'start_date' => @transaction.start_date,
+                        'sales_rep_name' => @transaction.sales_rep_name,
+                        'platform_customer_id' => customer_id},
+          }, stripe_account: connected_account.sid)
+
+          puts "***Charge ID: " + charge.id
+        rescue StandardError => e
+          flash[:error] = "Error: Transaction not completed. Charge failed. " + e.to_s
+          render :new
+          return
         end
-      rescue StandardError => e
-        flash[:error] = "Error: Transaction not completed. Customer update failed. " + e.to_s
-        render :new
-        return
       end
 
 
@@ -139,19 +115,19 @@ class TransactionsController < ApplicationController
       # Making tokens
       # If the customer is stored on the platform account and the charge is on the
       # connected account, a new token needs to be created
-      customer_token = nil
-      if (!is_subscription)
-        begin
-          customer_token = Stripe::Token.create({
-            :customer => customer_id,
-          }, {:stripe_account => connected_account.sid })
-          puts "New Customer Token: " + customer_token.id
-        rescue StandardError => e
-          flash[:error] = "Error: Transaction not completed. Customer token creation failed. " + e.to_s
-          render :new
-          return
-        end
-      else
+      # customer_token = nil
+      # if (!is_subscription)
+      #   begin
+      #     customer_token = Stripe::Token.create({
+      #       :customer => customer_id,
+      #     }, {:stripe_account => connected_account.sid })
+      #     puts "New Customer Token: " + customer_token.id
+      #   rescue StandardError => e
+      #     flash[:error] = "Error: Transaction not completed. Customer token creation failed. " + e.to_s
+      #     render :new
+      #     return
+      #   end
+      # else
 
         # # Save the customer on the connected account
         # # https://stripe.com/docs/connect/shared-customers#making-a-charge
@@ -167,6 +143,8 @@ class TransactionsController < ApplicationController
         # Signing up the customer for the installment plan
         # Do this only if the customer has selected option (2)
         # Customer is automatically charged
+      if (is_subscription)
+        # TODO: setup a Product and a Plan
         begin
           subscription = Stripe::Subscription.create({
               customer: customer_id,
@@ -190,36 +168,7 @@ class TransactionsController < ApplicationController
         end
       end
       
-      # Charge the customer
-      # Testing errors:
-      # https://stripe.com/docs/testing
-      # Receipts: https://stripe.com/docs/receipts
-      # https://stripe.com/docs/recipes/sending-custom-email-receipts
-      # Metadata: https://stripe.com/docs/charges
-      begin
-        charge = Stripe::Charge.create({
-          amount: (@transaction.price * 100).to_i,
-          currency: "usd",
-          description: @transaction.item,
-          source: customer_token.id,
-          statement_descriptor: @transaction.item[0..21],
-          receipt_email: @transaction.email,
-          application_fee_amount: (application_fee * 100).to_i,
-          # customer: shared_customer.id,
-          metadata: {'name' => @transaction.name,
-                      'email' => @transaction.email,
-                      'phone' => @transaction.phone,
-                      'start_date' => @transaction.start_date,
-                      'sales_rep_name' => @transaction.sales_rep_name,
-                      'platform_customer_id' => customer_id},
-        }, stripe_account: connected_account.sid)
-
-        puts "***Charge ID: " + charge.id
-      rescue StandardError => e
-        flash[:error] = "Error: Transaction not completed. Charge failed. " + e.to_s
-        render :new
-        return
-      end
+      
 
       @transaction.charge_id = charge["id"]
       @transaction.save
@@ -233,8 +182,77 @@ class TransactionsController < ApplicationController
 
   private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def transaction_params
-      params.require(:transaction).permit(:name, :email, :phone, :start_date, :sales_rep_name, :item, :connected_account_id)
+  # Create a Customer on Connected Account
+  def create_or_update_customer(name, email, phone, sales_rep_name, stripe_token, sid)
+    # finding if customer exists in connected account
+    customer_id = nil
+    begin
+      customers = Stripe::Customer.list({
+          email: email,
+          limit: 1
+      }, stripe_account: sid)
+
+      if (customers["data"].size == 0)
+        # Customer not found
+        # Create a new Customer on Connected Account
+        puts "Creating new Customer"
+        customer = Stripe::Customer.create({
+          name: name,
+          email: email,
+          phone: phone,
+          source: stripe_token, # obtained with Stripe.js
+        }, stripe_account: sid)
+        customer_id = customer["id"];
+        puts "New Customer ID: " + customer_id
+      else
+        # Customer already exists, get the Customer ID
+        puts "Customer #{name} already exists"
+        customer_id = customers["data"][0]["id"]
+        puts "Customer ID: " + customer_id
+        # Update Name, Phone and Credit Card info (source)
+        Stripe::Customer.update(
+          customer_id,
+          {
+            name: name,
+            email: email,
+            phone: phone,
+            source: stripe_token # obtained with Stripe.js
+          }, stripe_account: sid)
+        puts "Customer Updated"
+      end
+    rescue StandardError => e
+      flash[:error] = "Error: Transaction not completed. Customer update failed. " + e.to_s
+      render :new
+      return
     end
+    return customer_id
+  end
+
+  def calculate_stripe_fee(price)
+    return (0.3 + (2.9 / 100.0) * price).round(2)
+  end
+
+  def calculate_application_fee(price, commission)
+    # Determine the Application Fee amount
+    client_received = price - calculate_stripe_fee(price)
+    if (client_received <= 0)
+      redirect_to connected_accounts_url, flash: { error: "Client made negative amount so no Application Fee received."}
+      return
+    end
+    af = ((commission / 100.0) * client_received).round(2)
+    return af
+  end
+
+  def direct_charge
+  end
+
+  def destination_charge
+  end
+
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+  def transaction_params
+    params.require(:transaction).permit(:name, :email, :phone, :start_date, :sales_rep_name, :item, :connected_account_id)
+  end
 end
