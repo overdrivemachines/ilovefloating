8a4c13e01ee586782224ba0ae52ae364640df284
Dipen Chauhan <get.dipen@gmail.com>
2019-07-07 19:38:09 -0700

    Added subscription

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -20,7 +20,7 @@ class TransactionsController < ApplicationController
   # POST /transactions
   def create
     @transaction = Transaction.new(transaction_params)
-
+    is_subscription = false
     # Determining item name and price
     # TODO: move items to a table
     if (transaction_params[:item].nil?)
@@ -33,6 +33,10 @@ class TransactionsController < ApplicationController
       @transaction.item = "60 Minute Floatation Therapy Session"
       @transaction.price = 75.0
     elsif (transaction_params[:item] == "2")
+      @transaction.item = "6 Week Stress Release Program (Payment Plan)"
+      @transaction.price = 75.0
+      is_subscription = true
+    elsif (transaction_params[:item] == "3")
       @transaction.item = "Test"
       @transaction.price = 2.0
     end
@@ -66,7 +70,7 @@ class TransactionsController < ApplicationController
       # Determine the Connected Account's 
 
       # Stripe.api_key = 'sk_test_lPVHKFQDPSSLI6uiJr4dVdY7'
-      Stripe.api_key = Rails.application.credentials.stripe[:development][:secret_key]
+      Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:secret_key]
       # Stripe.api_key = Rails.application.credentials.stripe[:production][:secret_key]
       # Stripe.api_key = Rails.application.credentials.api_key
 
@@ -88,7 +92,7 @@ class TransactionsController < ApplicationController
           puts "New Customer ID: " + customer_id
         else
           # Customer already exists, get the Customer ID
-          puts "Customer already exists"
+          puts "Customer #{@transaction.name} already exists"
           customer_id = customers["data"][0]["id"]
           puts "Customer ID: " + customer_id
           # Update Name and Phone
@@ -98,7 +102,7 @@ class TransactionsController < ApplicationController
               name: @transaction.name,
               email: @transaction.email,
               phone: @transaction.phone,
-              source: stripe_token
+              source: stripe_token # obtained with Stripe.js
             })
           puts "Customer Updated"
         end
@@ -116,14 +120,43 @@ class TransactionsController < ApplicationController
         customer_token = Stripe::Token.create({
           :customer => customer_id,
         }, {:stripe_account => connected_account.sid })
-        puts "New Customer Token: " + customer_token.to_s
+        puts "New Customer Token: " + customer_token.id
       rescue StandardError => e
         flash[:error] = "Error: Transaction not completed. Customer token creation failed. " + e.to_s
         render :new
         return
       end
 
+      # Signing up the customer for the installment plan
+      # Do this only if the customer has selected option (2)
+      # Customer is automatically charged
+      if (is_subscription)
+        begin
+          subscription = Stripe::Subscription.create({
+              customer: customer_id,
+              items: [{
+                  plan: Rails.application.credentials.stripe[Rails.env.to_sym][:weekly_plan_id],
+              }],
+              application_fee_percent: connected_account.commission,
+              metadata: {
+                  installments_paid: 0,
+              },
+          })
+          puts "Subscription: " + subscription.to_s
+          @transaction.charge_id = subscription["id"]
+          @transaction.save
+          redirect_to connected_accounts_url, flash: { success: "Transaction completed successfully. Subscription ID: #{subscription["id"]}. Amount: $#{@transaction.price}. Stripe Fees: $#{stripe_fee}. Application Fee: $#{application_fee}"}
+          return
+        rescue StandardError => e
+          flash[:error] = "Error: Transaction not completed. Charge failed. " + e.to_s
+          render :new
+          return
+        end
+      end
 
+      asdf
+      
+      # Charge the customer
       # Testing errors:
       # https://stripe.com/docs/testing
       # Receipts: https://stripe.com/docs/receipts
@@ -134,7 +167,7 @@ class TransactionsController < ApplicationController
           amount: (@transaction.price * 100).to_i,
           currency: "usd",
           description: @transaction.item,
-          source: customer_token.id,
+          # source: customer_token.id,
           statement_descriptor: @transaction.item[0..21],
           receipt_email: @transaction.email,
           application_fee_amount: (application_fee * 100).to_i,
@@ -146,7 +179,7 @@ class TransactionsController < ApplicationController
                       'sales_rep_name' => @transaction.sales_rep_name },
         }, stripe_account: connected_account.sid)
       rescue StandardError => e
-        flash[:error] = "Error: Transaction not completed. " + e.to_s
+        flash[:error] = "Error: Transaction not completed. Charge failed. " + e.to_s
         render :new
         return
       end
