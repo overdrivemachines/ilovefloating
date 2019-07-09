1d3110b45a39f1928f306ba49b2a60324d1a3095
Dipen Chauhan <get.dipen@gmail.com>
2019-07-09 00:41:22 -0700

    Checking for livemode

--- app/services/stripe/invoice_event_handler.rb
+++ app/services/stripe/invoice_event_handler.rb
@@ -28,44 +28,56 @@ module Stripe
       # https://stripe.com/docs/recipes/installment-plan
       # https://stripe.com/docs/webhooks/setup
       
-      # Grab the subscription line item.
-      sub =  event.data.object.lines.data[0]
+      puts "Livemode: " + event["livemode"].to_s
 
-      # Execute only for installment plans.
-      if !sub.metadata[:installments_paid].nil?
-      # Recommendation: Log invoices and check for duplicate events.
-      # Recommendation: Note that we send $0 invoices for trials.
-      #                 You can verify the `amount_paid` attribute of
-      #                 the invoice object before incrementing the count.
+      if (event["livemode"] == false)
+        # Grab the subscription line item.
+        sub =  event.data.object.lines.data[0]
+
+        puts "***event.data.object.lines.data[0]: " + sub.to_s
+
+        return
+
+        # Execute only for installment plans.
+        if !sub.metadata[:installments_paid].nil?
+          # Recommendation: Log invoices and check for duplicate events.
+          # Recommendation: Note that we send $0 invoices for trials.
+          #                 You can verify the `amount_paid` attribute of
+          #                 the invoice object before incrementing the count.
 
           # Retrieve and increment the number of payments.
           count = sub.metadata[:installments_paid].to_i
           count += 1
           # Metadata is not write-protected; creating a database is an alternative.
 
+          puts "New Installment: " + count.to_s
           # Save incremented value to `installments_paid` metadata of the subscription.
-          subscription_object = Stripe::Subscription.update(
-            sub[:subscription],
-            {
+          
+          # begin
+          subscription_object = Stripe::Subscription.update( 
+            sub["subscription"]["id"], {
               metadata: {
-                installments_paid: count,
+                  installments_paid: count,
+                  source: "RoR"
               },
-            }
-          )
+            }, stripe_account: event["account"] )
+
+          puts "Subscription " + sub["subscription"]["id"] + " updated. Count: " + count
 
           # Check if all 10 installments have been paid.
           # If paid in full, then cancel the subscription.
-          if count >= 10
+          if count >= 6
               subscription_object.delete
           end
+          # rescue StandardError => e
+          #   render json: {:status => 400, :error => e}
+          #   return
+          # end
+          
+        end
       end
-
-
-
-
-
-      puts event.data.object.class
-      render json: {:status => 400, :info => event}
+      # puts event.data.object.class
+      # render json: {:status => 400, :info => event}
     end
   end
 end
