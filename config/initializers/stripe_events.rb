dd67f5688bdc761ee9275709ad6a15a1af35bc6b
Dipen Chauhan <get.dipen@gmail.com>
2019-07-10 10:34:34 -0700

    Using secret_key

--- config/initializers/stripe_events.rb
+++ config/initializers/stripe_events.rb
@@ -1,4 +1,4 @@
-Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:publishable_key]
+Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:secret_key]
 StripeEvent.signing_secret = Rails.application.credentials.stripe[Rails.env.to_sym][:signing_secret]
 
 StripeEvent.configure do |events|
